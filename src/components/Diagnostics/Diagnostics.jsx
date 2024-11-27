7209521680f4653a97b429680129188b3ff3f1f2
Ziyoda Usmonjonova <ziyodausmonjonova1@gmail.com>
2024-11-27 23:03:59 +0500

    finish diagnostics

--- src/components/Diagnostics/Diagnostics.jsx
+++ src/components/Diagnostics/Diagnostics.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import "./Diagnostics.css";
+import React from 'react'
 
-const Diagnostics = ({ description }) => (
-    <section className="diagnostics">
-        <h2>Psixologik Diagnostika</h2>
-        <p>{description}</p>
-    </section>
-);
+const Diagnostics = () => {
+    return (
+        <section className="knowledge-test bg-[#F5F5F5] py-6">
+            <div className="container px-4 py-8">
+                <h2 className="pl-6 mb-8 relative text-4xl font-semibold before:content-[''] before:absolute before:w-[13px] before:h-[43px] before:bg-[#0956AF] before:top-[-4px] before:left-0 after:content-[''] after:absolute after:w-[843px] after:h-[4px] after:bg-[#DEE2E6] after:top-5 after:right-0">
+                    Психологик диагностика
+                </h2>
+                <div className='text-[ rgb(0, 0, 0)] text-xl line flex gap-6 mt-8'> 
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make  a type specimen book. It has survived not only five centuries, </p>
+                    <p>  a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of</p>
+                </div>
+            </div>
+        </section>
+    )
+}
 
-export default Diagnostics;
+export default Diagnostics
